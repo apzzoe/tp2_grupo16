8a5628321e0732d657f2cbf4d63edfc507ad8335
apzzoe <apazazoe8@gmail.com>
2026-08-25 19:09:18 -0300

    resolucion de ejercicio 3 - Zelaya Apaza Zoe

--- tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio3/Ejercicio3.java
+++ tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio3/Ejercicio3.java
@@ -3,8 +3,15 @@ package ar.edu.unju.escmi.tp2.ejercicio3;
 public class Ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		final int CODIGO_POSTAL_JUJUY = 4600;
+		final double PI = 3.1415926535;
+		final int DIAS_SEMANA = 7;
+		final int MESES_ANIO = 12;
+		
+		System.out.println("codigo postal de la provincia de jujuy: " + CODIGO_POSTAL_JUJUY);
+		System.out.println("valor matematico de  π: " + PI);
+		System.out.println("cantidad de dias de la semana: " + DIAS_SEMANA);
+		System.out.println("cantidad de meses que tiene un año: " + MESES_ANIO);
 	}
 
 }
