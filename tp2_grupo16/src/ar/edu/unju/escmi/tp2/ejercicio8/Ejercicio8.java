5fdb9076b7466b966f74ec10b3bce1486d78589e
apzzoe <apazazoe8@gmail.com>
2026-08-25 19:43:44 -0300

    resolucion de ejercicio 8 - Zelaya Apaza Zoe

---
+++ tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio8/Ejercicio8.java
@@ -0,0 +1,26 @@
+package ar.edu.unju.escmi.tp2.ejercicio8;
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+		int a=5, b=10, c=2, d=6;
+		
+		System.out.println("variables antes de usar los operadores:");
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("c: " + c);
+		System.out.println("d: " + d);
+		
+		a+=5;
+		b-=2;
+		c*=3; 
+		d/=2; 
+		
+		System.out.println("resultados de las operaciones:");
+		System.out.println("a: " + a);
+		System.out.println("b: " + b);
+		System.out.println("c: " + c);
+		System.out.println("d: " + d);
+	}
+
+}
