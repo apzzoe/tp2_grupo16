af7585add56ae08ac7450b85e6430526cfc46a6b
Meliip414 <mmalexandra2019@hotmail.com>
2026-08-26 19:20:13 -0300

    Agregado Ejercicio4 - Leaño Melina

---
+++ tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio4/Ejercicio4.java
@@ -0,0 +1,16 @@
+package ar.edu.unju.escmi.tp2.ejercicio4;
+
+import java.lang.Math;
+public class Ejercicio4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 3; 
+		double resultado; 
+		
+		resultado = Math.pow((num*(num+1)/2),2);
+		System.out.println("el resultado para num = 3 es:  " + resultado);
+
+	}
+
+}
