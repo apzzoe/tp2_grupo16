ad0afe3c55b4695ab2a64f3dea6379f55e6a7e85
apzzoe <apazazoe8@gmail.com>
2026-08-25 22:02:48 -0300

    resolucion de ejercicio 18 - Zelaya Apaza Zoe

---
+++ tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio18/Ejercicio18.java
@@ -0,0 +1,27 @@
+package ar.edu.unju.escmi.tp2.ejercicio18;
+
+public class Ejercicio18 {
+
+	public static void main(String[] args) {
+		Integer num1 = 1000;
+		Integer num2 = 1000;
+
+		System.out.println("num1: " + num1);
+		System.out.println("num2: " + num2);
+		
+		if (num1==num2) {
+			System.out.println("las referencias son iguales");
+		}
+		else {
+			System.out.println("las referencias no son iguales");
+			
+		}
+		
+		if (num1.equals(num2)) {
+            System.out.println("los valores son iguales");
+        } else {
+            System.out.println("los valores no son iguales");
+        }
+	}
+
+}
