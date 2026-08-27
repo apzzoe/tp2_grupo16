383875b216cac3c5b142380ffb43e9be48852629
BarbaraVillena <barbimorevi@gmail.com>
2026-08-26 23:11:29 -0300

    Ejercicio 1 - Villena Luisana

---
+++ tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio1/Ejercicio1.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.escmi.tp2.ejercicio1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		int DNI;
+		char letra;
+		int edad;
+		String nombreProcer;
+		int numBingo;
+		double precio;
+		double estatura;
+		double pesoMascota;
+		String fechaInicioCuatri;
+		String fechaFinalCuatri;
+
+	}
+
+}
