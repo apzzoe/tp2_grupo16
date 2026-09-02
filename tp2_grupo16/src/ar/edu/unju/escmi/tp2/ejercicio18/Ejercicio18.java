6bf882ccae1382954e646c5a1a16582660834d1b
apzzoe <apazazoe8@gmail.com>
2026-09-02 13:59:22 -0300

    Corrección de ejercicio 18 - Zelaya Apaza Zoe

--- tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio18/Ejercicio18.java
+++ tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio18/Ejercicio18.java
@@ -6,8 +6,8 @@ public class Ejercicio18 {
 		Integer num1 = 1000;
 		Integer num2 = 1000;
 
-		System.out.println("num1: " + num1);
-		System.out.println("num2: " + num2);
+		System.out.println("referencia de memoria de num1: " + System.identityHashCode(num1));
+		System.out.println("referencia de memoria de num2: " + System.identityHashCode(num2));
 		
 		if (num1==num2) {
 			System.out.println("las referencias son iguales");
