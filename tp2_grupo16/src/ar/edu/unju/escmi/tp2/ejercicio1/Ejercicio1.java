6e1378ec87f60e0139575c63010a52b7b17471bd
BarbaraVillena <barbimorevi@gmail.com>
2026-09-04 18:04:32 -0300

    Correccion ejercicio1 - Villena Luisana Barbara

--- tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio1/Ejercicio1.java
+++ tp2_grupo16/src/ar/edu/unju/escmi/tp2/ejercicio1/Ejercicio1.java
@@ -3,15 +3,26 @@ package ar.edu.unju.escmi.tp2.ejercicio1;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		int dni;
-		char letra;
-		int edad;
-		String nombreProcer;
-		int numBingo;
-		double precio;
-		double estatura;
-		double pesoMascota;
-		String fechaInicioCuatri;
-		String fechaFinalCuatri;
+		int dni=48142385;
+		char letra='a';
+		int edad=19;
+		String nombreProcer="Juana Azurduy";
+		int numBingo=1020;
+		double precio=10.99;
+		double estatura=158.8;
+		double pesoMascota=12.8;
+		String fechaInicioCuatri="10/03/2026";
+		String fechaFinalCuatri="12/07/2026";
+		
+		System.out.println("DNI(entero): " + dni);
+		System.out.println("Una letra(caracter): " + letra);
+		System.out.println("Mi edad(entero): " + edad);
+		System.out.println("Procer(cadena): " + nombreProcer);
+		System.out.println("Numero de bingo(entero): " + numBingo);
+		System.out.println("Precio(decimal): " + precio);
+		System.out.println("Estatura(decimal): " + estatura);
+		System.out.println("Peso de mi mascota(decimal) : " + pesoMascota);
+		System.out.println("Mi estatura(decimal): " + fechaInicioCuatri);
+		System.out.println("Inicio de cuatrimestre(cadena): " + fechaFinalCuatri);
 	}
 }
